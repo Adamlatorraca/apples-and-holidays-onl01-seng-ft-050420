8b7164e74921034d17cdf1f0a8a706ead6636a8e
Adam LaTorraca <adam.latorraca@gmail.com>
2020-05-09 00:01:40 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -13,6 +13,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   holiday_hash.each do |season, holiday|
     if season == :winter
       holiday.each do |day, input|
+        input << supply
     end
   end
 end
