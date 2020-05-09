5591ebcf1dcae2044b8bf6ca8eb51585db988c33
Adam LaTorraca <adam.latorraca@gmail.com>
2020-05-09 00:44:08 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -38,7 +38,7 @@ def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
   holiday_hash.each do |season, holiday|
     if season == :winter
-      all_supplies[holiday]
+      holiday.values
     end
 end
 
